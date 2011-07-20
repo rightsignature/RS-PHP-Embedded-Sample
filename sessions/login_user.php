6982165e34cfb8f831cf1845a4a80cafb700e423
Alex Chee <alexchee11@gmail.com>
2011-07-19 18:02:54 -0700

    fixed typo when logging in

--- sessions/login_user.php
+++ sessions/login_user.php
@@ -5,7 +5,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/config/common.inc.php");
 $user = new User();
 if ($user->load("email=\"" . mysql_escape_string(trim($_POST["user_email"])) . "\"")) {
 	$_SESSION["user_id"] = $user->id;
-	$_SESSION["success_message"] = "Successfully Logged Out";
+	$_SESSION["success_message"] = "Successfully Logged In";
 	header("Location: $siteURL/");
 } else {
 	$_SESSION["error_message"] = "Error Logging In, please make sure the information is correct.";
